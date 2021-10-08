9bedc756e41dac1c4a0c8a60af703eaac2fd494a
navdeep <navdepdhakar1@gmai.com>
2021-10-08 09:15:10 +0530

    basic styling change

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1>Wapp</h1>
   {#if $username}
     <div class="user-bio">
       
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3>Decentralized Chat</h3>
   {/if}
 </header>
